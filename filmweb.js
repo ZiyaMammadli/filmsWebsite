fd52b2e85464f5a77014f4f5f19f686fe546fe23
Your Name <you@example.com>
2024-02-27 11:33:10 +0400

    ShowCard method

--- filmweb.js
+++ filmweb.js
@@ -34,3 +34,43 @@ function GetCards(){
 GetCards()
 
 
+let Form=document.getElementById("form");
+let SearchInput=document.getElementById("searchInput");
+
+Form.addEventListener("submit",AddCard)
+
+function AddCard(e){
+  e.preventDefault()
+  const inputValue=SearchInput.value.trim()
+  fetch(`https://api.tvmaze.com/shows`)
+    .then(response=>response.json())
+    .then((datas)=>{
+        datas.forEach(data => {
+          console.log(inputValue.lentgh);
+          if(inputValue.toLowerCase()==data.name.toLowerCase()){
+            card.innerHTML=`       <div class="col-3">
+            <div class="card" style="width: 18rem; margin-top: 100px;">
+                <img src="${data.image.medium}" class="card-img-top" alt="Sekil tapilmadi">
+                <div class="card-body">
+                  <h5 class="card-title">${data["name"]}</h5>
+                  <p class="card-text">Premiere: ${data["premiered"]}</p>
+                </div>
+                <ul class="list-group list-group-flush">
+                  <li class="list-group-item">IMDB Rating: ${data.rating.average}</li>
+                  <li class="list-group-item">Genre: ${data.genres}</li>
+                  <li class="list-group-item">Language: ${data["language"]}</li>
+                </ul>
+                <div class="card-body" style="display: flex;justify-content: space-between">
+                <button class="btn btn-outline-success" id="btn1" type="submit"style="background-color: #DBDCDA;">
+                                <a href="${data["officialSite"]}" style="text-decoration: none; color: black;">Go to website</a>
+                </button>
+                <button class="btn btn-outline-success" id="btn2" type="submit" style="background-color: #DBDCDA;">
+                                <a href="detail.html?detailsId=${data.id}" style="text-decoration: none; color: black;">Go to Detail</a>
+                </button>
+                </div>
+              </div>
+        </div>`
+          }
+        });
+    })
+}
